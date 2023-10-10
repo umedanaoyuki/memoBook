3c584eeb4c660bf95b82db8ef46ff927128ef74f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-11 00:11:48 +0900

    削除処理

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 
     const text = document.querySelector("#text");
     const save = document.querySelector("#save");
+    const clear = document.querySelector('#clear')
     const message = document.querySelector("#message");
 
     if (localStorage.getItem('memo') === null) {
@@ -10,7 +11,14 @@
     } else {
         text.value = localStorage.getItem('memo');
     }
-    
+
+    clear.addEventListener('click', () => {
+        if (confirm('本当に削除しますか？') === true) {
+            text.value = '';
+            localStorage.removeItem('memo');
+        }
+    })
+
     save.addEventListener('click', () => {
         message.classList.add('appear');
         setTimeout( () => {
@@ -18,6 +26,8 @@
         }, 1000)
         localStorage.setItem('memo', text.value);
     });
+
+
 
 
 }
