b4705b36be23e9f78300ab4eb86597b3b86e79fd
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-10 23:50:14 +0900

    アニメーションの追加

--- main.js
+++ main.js
@@ -3,8 +3,13 @@
 
     const text = document.querySelector("#text");
     const save = document.querySelector("#save");
+    const message = document.querySelector("#message");
 
     save.addEventListener('click', () => {
-        alert(text.value)
+        message.classList.add('appear');
+        setTimeout( () => {
+            message.classList.remove('appear');
+        }, 1000)
+        localStorage.setItem('memo', text.value);
     });
 }
